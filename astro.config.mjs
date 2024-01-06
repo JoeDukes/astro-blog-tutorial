4bf54bb048139941a41f89b96c6feb8706389d0b
Joe Dukes (S1oop) <joedukes@gmail.com>
2024-01-05 22:45:40 -0500

    Add Preact to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://s1oop-astro-blog-tutorial.netlify.app"
+  site: "https://s1oop-astro-blog-tutorial.netlify.app",
+  integrations: [preact()]
 });
